095673df6292cf7c93f32d16e890b9f61e54cbe5
Jhon Perez <juanp20077@gmail.com>
2025-10-02 15:47:40 -0500

    Disimbiguate file names and avoid unnecessary subscripting
    looking at multiple mod.rs and main.rs files can get confusing, so add
    the parent folder to the file name.

--- .github/code-to-latex.py
+++ .github/code-to-latex.py
@@ -15,11 +15,23 @@ def getRootsAndFiles(dirname):
             rootsAndFiles.append((root, file))
     return rootsAndFiles
 
+# Rust uses multiple mod.rs files which can be confusing to differentiate
+# Add the name of the folder in which the file is in
+def disimbiguate_file(root: str, file):
+    if "mod.rs" in file or "main.rs" in file:
+        last_root = root.rfind("/")
+        file = root[last_root + 1:] + "/" + file
+
+    return file
+
 def inputFile(root, file, output):
-    output.write(f"\subsection{{{file}}}\n")
-    output.write(f"\lstinputlisting[language=Rust]{{{root + '/' + file}}}\n")
+    fixed_file = disimbiguate_file(root, file)
+    # use escape sequence to avoid subscripting with _
+    fixed_file = file.replace("_", "\\_")
+    output.write(f"\\subsection{{{fixed_file}}}\n")
+    output.write(f"\\lstinputlisting[language=Rust]{{{root + '/' + file}}}\n")
     
-def handleDirs(dirname, output, keywords = []):
+def handleDirs(dirname, output):
     files = getRootsAndFiles(dirname)
     for root, file in sorted(files):
         inputFile(root, file, output)
